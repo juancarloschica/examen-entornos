cd31541d08b7d52c9445a9abb4ee791cd4642960
Juan Carlos Chica Ureña <juancarlos.boxing@gmail.com>
2018-12-11 14:39:40 +0100

    documentacion commit

--- Calcular.java
+++ Calcular.java
@@ -7,17 +7,28 @@
  * */
 public class Calcular {
 	
-	/** @return El área del triángulo */
+	/** 
+	 * @param baseTri base del triángulo 
+	 * @param alturaTri altura del triángulo 
+	 * @return El área del triángulo */
 	public static double areaTriangulo(int baseTri, int alturaTri) {
 		return baseTri * alturaTri / 2;
 	}
 	
-	/** @return El área del rectángulo */
+	/**
+	 * @param baseRec base del rectángulo 
+	 * @param alturaRec altura del rectángulo 
+	 * @return El área del rectángulo */
 	public static int areaRectangulo(int baseRec, int alturaRec) {
 		return baseRec * alturaRec;
 	}
 	
-	/** @return El área del trapecio */
+	/** 
+	 * @param baseTraA primera base del trapecio 
+	 * @param baseTraB segunda base del trapecio 
+	 * @param alturaTra altura del trapecio
+	 * @param m mediana del trapecio
+	 * @return El área del trapecio */
 	public static double areaTrapecio(int baseTraA, int baseTraB, int alturaTra) {
 			int m = (baseTraA + baseTraB) / 2;
 		return alturaTra * m;
